eb616e2b3adde43509c843c93d94625369619615
Refat <nobilix@gmail.com>
2018-02-21 17:06:31 +0300

    package json and bundler config

---
+++ config/webpack.config.base.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var outputFile = 'vue-api-service'
+var globalName = 'VueApiService'
+
+var config = require('../package.json')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ExtractTextPlugin.extract('css-loader'),
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'VERSION': JSON.stringify(config.version),
+    })
+  ],
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+export { default } from './dist/vue-api-service.common'
+export * from './dist/vue-api-service.common'
